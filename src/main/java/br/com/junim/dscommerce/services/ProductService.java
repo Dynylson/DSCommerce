d1ec3df688860ea079c622f91b4a24d403ca8b20
Dynylson Junior <dynylsonjr@gmail.com>
2023-12-22 22:48:41 -0300

    GET Products paged

--- src/main/java/br/com/junim/dscommerce/services/ProductService.java
+++ src/main/java/br/com/junim/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import br.com.junim.dscommerce.dto.ProductDTO;
 import br.com.junim.dscommerce.entities.Product;
 import br.com.junim.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,13 @@ public class ProductService {
         Product product = repository.findById(id).get();
 
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
